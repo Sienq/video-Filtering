c4993c3e9307e2320f6c00c5355115f0f1eff6fb
sienq <Hubert.Sienkiewicz99@gmail.com>
2020-10-16 23:33:49 +0200

    Aded Edge detection live filter

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import tkinter
 import cv2
 import MyVideoCapture
 import PIL.Image,PIL.ImageTk
+import filters
 
 class App:
     def __init__(self,window,window_name,videoSource = 0):
@@ -18,6 +19,14 @@ class App:
         self.buttonScreen = tkinter.Button(self.window,text='Screenshot',width=50,command = self.snapshot)
         self.buttonScreen.pack(anchor=tkinter.CENTER,expand = True)
 
+        self.buttonEdge = tkinter.Button(self.window,text = 'Edge Detection',width = 50,command = self.edgeFilter)
+        self.buttonEdge.pack(anchor=tkinter.CENTER,expand = True)
+
+        self.buttonEdge = tkinter.Button(self.window,text = 'Normal',width = 50,command = self.normal)
+        self.buttonEdge.pack(anchor=tkinter.CENTER,expand = True)
+
+        self.flag = 0
+
         self.delay = 15
         self.update()
 
@@ -27,9 +36,22 @@ class App:
         if self.ret:
             cv2.imwrite('screenshot.png',cv2.cvtColor(self.frame,cv2.COLOR_RGB2BGR))
 
+
+    def edgeFilter(self):
+        self.flag = 1
+
+    def normal(self):
+        self.flag = 0
+
     def update(self):
 
         self.ret,self.frame = self.video.getFrame()
+        if self.flag == 0:
+            self.ret,self.frame = self.video.getFrame()
+
+        if self.flag == 1:
+            self.frame = filters.customEdgeDetection((3,3),cv2.cvtColor(self.frame,cv2.COLOR_RGB2BGR))
+            self.frame = cv2.cvtColor(self.frame,cv2.COLOR_BGR2RGB)
 
         if self.ret:
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.frame))
